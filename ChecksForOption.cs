d5ca0a60898ed95f41603843dde47cb7efa9e576
bindhu97 <binduvishwakarma97@gmail.com>
2022-12-06 11:43:01 +0530

    Checks for Option

---
+++ ChecksForOption.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder1
+{
+    internal class ChecksForOption
+    {
+        public void checkoption()
+        {
+            int position = 0;
+            Random rand = new Random();
+            int diceValue = (int)(rand.NextInt64(6) + 1);
+            Console.WriteLine("Dice Value is " + diceValue);
+
+
+            int option = (int)rand.NextInt64(3);
+            if (option == 0)
+            {
+                Console.WriteLine("No Play");
+            }
+            else if (option == 1)
+            {
+                Console.WriteLine("Ladder");
+                position += diceValue;
+                Console.WriteLine("Current Position is " + position);
+            }
+            else
+            {
+                Console.WriteLine("Snake");
+                position -= diceValue;
+                Console.WriteLine("Current Position is " + position);
+            }
+        }
+    }
+}
